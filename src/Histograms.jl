ec19ab4f149b4607147bfe6b2983cf845f368cbc
C.Brenhin Keller <cbkeller@dartmouth.edu>
2021-02-17 22:20:19 -0500

    Special case for integers

--- src/Histograms.jl
+++ src/Histograms.jl
@@ -5,18 +5,32 @@ function histcounts(x::AbstractArray, xmin, xmax, nbins; T=Int64)
 end
 export histcounts
 
-function histcounts!(N, x, xmin, xmax, nbins)
+function histcounts!(N::Array, x::AbstractArray, xmin, xmax, nbins)
     # What is the size of each bin?
     binwidth = (xmax-xmin)/nbins
 
     # Loop through each element of x
     @inbounds for i ∈ eachindex(x)
         xᵢ = x[i]
-        if !isnan(xᵢ)
+        if xᵢ==xᵢ # If not a NaN
             binindex = Integer((xᵢ - xmin) ÷ binwidth) + 1
             if 1 <= binindex <= nbins
                 N[binindex] += 1
             end
+        end
+    end
+    return N
+end
+function histcounts!(N::Array, x::AbstractArray{<:Integer}, xmin, xmax, nbins)
+    # What is the size of each bin?
+    binwidth = (xmax-xmin)/nbins
+
+    # Loop through each element of x
+    @inbounds for i ∈ eachindex(x)
+        xᵢ = x[i]
+        binindex = Integer((xᵢ - xmin) ÷ binwidth) + 1
+        if 1 <= binindex <= nbins
+            N[binindex] += 1
         end
     end
     return N
