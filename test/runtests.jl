4f11b0cf0d3f3108e13718635b0b860af6b1dd0f
C.Brenhin Keller <cbkeller@dartmouth.edu>
2021-06-14 01:11:55 -0400

    Add tests for covariance and correlation

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,7 @@ using NaNStatistics
 using Test, Statistics, LinearAlgebra
 
 @testset "ArrayStats" begin include("testArrayStats.jl") end
+@testset "Covariance and Correlation" begin include("testCovCor.jl") end
+
 @testset "Histograms" begin include("testHistograms.jl") end
 @testset "Binning" begin include("testBinning.jl") end
