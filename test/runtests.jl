ca471eaef743f39390d3e4773d1d3a7d83570711
C.Brenhin Keller <cbkeller@dartmouth.edu>
2021-12-31 13:37:35 -0500

    Include sorting tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,7 @@ using Test, Statistics, LinearAlgebra
 
 @testset "ArrayStats" begin include("testArrayStats.jl") end
 @testset "Covariance and Correlation" begin include("testCovCor.jl") end
+@testset "Sorting" begin include("testSorting.jl") end
 
 @testset "Histograms" begin include("testHistograms.jl") end
 @testset "Binning" begin include("testBinning.jl") end
