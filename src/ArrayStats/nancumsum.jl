39798dfc2335c66e9fddf9bbe6535064b91e7bf1
C.Brenhin Keller <cbkeller@dartmouth.edu>
2022-07-16 23:34:43 -0400

    Add docstring for `nancumsum`

--- src/ArrayStats/nancumsum.jl
+++ src/ArrayStats/nancumsum.jl
@@ -1,4 +1,35 @@
+"""
+```julia
+nancumsum(A; dims)
+```
+Calculate the sum of an indexable collection `A`, ignoring NaNs, optionally
+along dimensions specified by `dims`.
 
+## Examples
+```julia
+julia> using NaNStatistics
+
+julia> A = [1 2; 3 4; NaN NaN]
+3×2 Matrix{Float64}:
+   1.0    2.0
+   3.0    4.0
+ NaN    NaN
+
+julia> nancumsum(A, dims=1)
+3×2 Matrix{Float64}:
+ 1.0  2.0
+ 4.0  6.0
+ 4.0  6.0
+
+julia> nancumsum(vec(A))
+6-element Vector{Float64}:
+  1.0
+  4.0
+  4.0
+  6.0
+ 10.0
+ 10.0
+"""
 nancumsum(A; dims=:) = _nancumsum(A, dims)
 export nancumsum
 
