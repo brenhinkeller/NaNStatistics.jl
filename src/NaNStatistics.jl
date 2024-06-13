29ed673aa8f8bf24ade027e506b8d25b31153640
C. Brenhin Keller <cbkeller@dartmouth.edu>
2024-06-12 21:07:33 -0400

    Remove LoopVectorization dependency

--- src/NaNStatistics.jl
+++ src/NaNStatistics.jl
@@ -1,15 +1,10 @@
 module NaNStatistics
 
-    # AVX vectorziation tools
-    using IfElse: ifelse
-    using LoopVectorization
-
     using Static
     const IntOrStaticInt = Union{Integer, StaticInt}
-    const PrimitiveFloat = Union{Float16, Float32, Float64}
-    const PrimitiveInteger = Union{Int8, Int16, Int32, Int64, Int128, UInt8, UInt16, UInt32, UInt64, UInt128}
-    const PrimitiveNumber = Union{PrimitiveFloat, PrimitiveInteger}
     _dim(::Type{StaticInt{N}}) where {N} = N::Int
+
+    using StaticArrayInterface: indices
 
     include("ArrayStats/ArrayStats.jl")
     include("ArrayStats/nanmean.jl")
